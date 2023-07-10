c947469a53c6012aa1525a270bc1cab9eaa32397
sivasankar <bssankar004@gmail.com>
2023-07-10 19:00:04 +0530

    Updates

---
+++ static/js/371.327a4a8f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmyapp=self.webpackChunkmyapp||[]).push([[371],{371:function(n,t,e){e.r(t),e.d(t,{default:function(){return r}});e(2791);var u=e(184);function r(){return(0,u.jsx)("div",{children:"ContentScreen ttt"})}}}]);
+//# sourceMappingURL=371.327a4a8f.chunk.js.map
